98f8cc61daa2bd709a60a8931c05324186789d58
filiberto <mo482616@gmail.com>
2022-02-09 09:02:46 -0600

    09-02-2022

---
+++ Clases/Clases/clase09-02-2022.R
@@ -0,0 +1,38 @@
+# Filiberto Lozoya Ojeda
+# 09/02/2022 
+# sesion semana 4 sala de computo
+
+
+# importar datos ----------------------------------------------------------
+
+vivero <- read.csv("Clases/BD_Viveros.csv", header = TRUE)
+
+
+# Estadistica Descriptiva -------------------------------------------------
+
+ # Medidas de Tendencia Central
+
+mean(BD_Viveros$IE)
+
+median(BD_Viveros$IE)
+
+range(BD_Viveros$IE)
+
+fivenum(BD_Viveros$IE)
+
+
+
+boxplot(BD_Viveros$IE)
+
+boxplot(BD_Viveros$IE, horizontal = T)
+
+boxplot(BD_Viveros$IE, horizontal = T, col = "orange")
+
+
+hist(BD_Viveros$IE, ylim = c(0, 12))
+
+table(BD_Viveros$IE)
+
+ftable(BD_Viveros$IE)
+
+frec <- table(BD_Viveros$IE)
